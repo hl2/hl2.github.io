fa5e4dc0ce499fe304e75c8c0c61146795e72808
Guillaume <grrseguin@gmail.com>
2017-05-04 10:50:02 +0200

    feat (bootstrap) customizing
    > https://v4-alpha.getbootstrap.com/getting-started/options/#customizing-variables
    > https://github.com/twbs/bootstrap/blob/v4-dev/scss/_variables.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,13 @@
 var gulp = require('gulp');
 var inject = require('gulp-inject');
-
-gulp.task(
-  'default',
-  [
-    'copy-css-vendor',
-    'copy-js-vendor',
-    'inject-dependencies',
-  ]
-);
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var moduleImporter = require('sass-module-importer');
 
 gulp.task('copy-css-vendor', function() {
   return gulp
     .src([
-      './node_modules/bootstrap/dist/css/bootstrap.min.css',
       './node_modules/animate.css/animate.min.css',
     ])
     .pipe(
@@ -59,4 +53,30 @@ gulp.task('inject-dependencies', function() {
     .pipe(
       gulp.dest('./_includes/')
     );
+});
+
+gulp.task('build-css-sass', function() {
+  return gulp.src(['./css/bootstrap/*.scss'])
+    .pipe(sass({
+      importer: moduleImporter()
+    }).on('error', sass.logError))
+    .pipe(cssmin())
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(gulp.dest('./css/vendor/'));
+});
+
+gulp.task(
+  'default', [
+    'build-css-sass',
+    'copy-css-vendor',
+    'copy-js-vendor',
+    'inject-dependencies',
+  ]
+);
+
+// watch for file changes, then run tasks assigned
+gulp.task('watch', function () {
+    gulp.watch('./css/bootstrap/*.scss', ['build-css-sass']);
 });
