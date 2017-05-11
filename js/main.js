d4efb31cb6b35d83f77c8793ce1d27bddde20bce
Guillaume <grrseguin@gmail.com>
2017-05-11 17:59:35 +0200

    feat (home) new version

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
   'use strict'; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a[href*="#"]:not([href="#"])').click(function() {
+  $('a[href*="#"]:not([href="#"]):not([data-toggle="collapse"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -28,50 +28,52 @@
 
   // Scroll reveal calls
   var hello = {
-    origin   : "top",
-    distance : "24px",
-    duration : 1500,
-    scale    : 1.05,
+    origin: 'top',
+    distance: '24px',
+    duration: 1500,
+    scale: 1.05,
   }
 
   var intro = {
-    origin   : "bottom",
-    distance : "64px",
-    duration : 900,
-    delay    : 1500,
-    scale    : 1,
+    origin: 'bottom',
+    distance: '64px',
+    duration: 900,
+    delay: 1500,
+    scale: 1,
   }
 
   var description = {
-    origin   : "top",
-    distance : "32px",
-    duration : 600,
-    delay    : 1800,
-    scale    : 0,
+    origin: 'top',
+    distance: '32px',
+    duration: 600,
+    delay: 1800,
+    scale: 0,
   }
   var block = {
     reset: true,
-    viewOffset: { top: 64 },
+    viewOffset: {
+      top: 64
+    },
   };
   var icon = {
-      duration: 600,
-      scale: 0.3,
-      distance: '0px',
+    duration: 600,
+    scale: 0.3,
+    distance: '0px',
   };
   var btn = {
-      duration: 1000,
-      delay: 200,
+    duration: 1000,
+    delay: 200,
   };
   var config = {
-    viewFactor : 0.15,
-    duration   : 800,
-    distance   : "0px",
-    scale      : 0.8,
+    viewFactor: 0.15,
+    duration: 800,
+    distance: '0px',
+    scale: 0.8,
   };
   window.sr = new ScrollReveal(config);
-  sr.reveal(".sr-hello", hello);
-  sr.reveal(".sr-intro", intro);
-  sr.reveal(".sr-hello .sr-desc", description);
+  sr.reveal('.sr-hello', hello);
+  sr.reveal('.sr-intro', intro);
+  sr.reveal('.sr-hello .sr-desc', description);
   sr.reveal('.sr-block', block, 200);
   sr.reveal('.sr-icons', icon, 200);
   sr.reveal('.sr-btn', btn);
